a34463bf0972c683175989d323ee473521db5675
hwthangg <hwthang.2510@gmail.com>
2025-03-26 14:47:10 +0700

    fix Tag

--- src/pages/SearchResult/Filter/Tag/Tag.jsx
+++ src/pages/SearchResult/Filter/Tag/Tag.jsx
@@ -1,43 +1,35 @@
-import "../Tag/Tag.css"; // Import file CSS cho component
+import { useState } from "react";
 
 const Tag = () => {
+    const tags = ["Hài hước", "Kinh dị", "Lãng mạn"]
+    const sorts = ["Thời gian cập nhật tăng dần", "Thời gian cập nhật giảm dần"]
+
+    const [tag, setTag] = useState(tags[0])
+    const [sort, setSort] = useState(sorts[0])
     return (
-        <div className="container-filter-tag">
-            {/* Khu vực chọn thể loại và sắp xếp */}
-            <div className="container-filter-tag-select">
-                {/* Chọn thể loại truyện */}
-                <div className="container-select">
-                    <label className="label" for="tag">Thể loại:</label>
-                    <select id="tag" name="tag">
-                        <option>Hài hước</option>
-                        <option>Kinh dị</option>
-                    </select>
-                </div>
-
-                {/* Chọn kiểu sắp xếp */}
-                <div className="container-select">
-                    <label className="label" for="sort">Sắp xếp:</label>
-                    <select id="sort" name="sort">
-                        <option>Thời gian cập nhật tăng dần</option>
-                        <option>Thời gian cập nhật giảm dần</option>
-                    </select>
-                </div>
+        <>
+        {/* container-tag */}
+        <div className=" grid grid-rows-2 md:grid-cols-2 bg-[#095533] md:gap-10 md:pb-1" >
+            {/* container-tag-filter */}
+            <div className=" md:row-span-2 flex flex-col justify-around mr-5">
+                <div className=" grid grid-cols-3 m-2">
+                <label className="text-white font-medium text-lg text-center">Thể loại:</label>
+                <select name="tag" id="tag" className=" col-span-2 bg-white rounded-md font-medium h-10" value={tag} onChange={(e) => setTag(e.target.value)}>
+                    {tags.map((tag,i)=>(<option key={i} value={tag}>{tag}</option>))}
+                </select></div>
+                <div className="grid grid-cols-3 justify-center m-2"> <label className="text-lg text-white font-medium text-center">Sắp xếp:</label>
+                <select name="sort" id="sort" className="md:h-10 col-span-2 bg-white rounded-md font-medium h-10" value={sort} onChange={(e) => setSort(e.target.value)}>
+                    {sorts.map((sort,i)=>(<option key={i} value={sort}>{sort}</option>))}
+                </select></div>
+               
+            </div>
+            {/* container-tag-description */}
+            <div className="md:border-l-2 md:border-white md:row-span-2 md:flex md:justify-center md:items-center m-2">
+                <p className="text-white p-5 text-justify ">Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản</p>
             </div>
 
-            {/* Mô tả về bộ lọc */}
-            <div className="container-filter-tag-description">
-                <p className="tag-description">
-                    It is a long established fact that a reader will be distracted by the readable 
-                    content of a page when looking at its layout. The point of using Lorem Ipsum is 
-                    that it has a more-or-less normal distribution of letters, as opposed to using 
-                    'Content here, content here', making it look like readable English. Many desktop 
-                    publishing packages and web page editors now use Lorem Ipsum as their default 
-                    model text, and a search for 'lorem ipsum' will uncover many web sites still in 
-                    their infancy. Various versions have evolved over the years, sometimes by 
-                    accident, sometimes on purpose (injected humour and the like).
-                </p>
-            </div>
         </div>
+        </>
     );
 };
 
