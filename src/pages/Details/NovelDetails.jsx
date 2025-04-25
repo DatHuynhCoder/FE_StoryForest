ec7a9e15415ca0ac688eb056215b87ff4282b3ea
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-04-26 01:07:40 +0700

    big update giao diện + chức năng comment

--- src/pages/Details/NovelDetails.jsx
+++ src/pages/Details/NovelDetails.jsx
@@ -9,7 +9,7 @@ function NovelDetails() {
   const navigate = useNavigate()
   //get user from redux store
   const user = useSelector((state) => state.user.user);
-  const { novelid } = useParams() // from NovelList.jsx
+  const { _id } = useParams() // from NovelList.jsx
   const [loading, setLoading] = useState(true)
   const [infoNovel, setInfoNovel] = useState({
     _id: '67eabb616f25807d87d7ad10',
@@ -38,6 +38,7 @@ function NovelDetails() {
       _id: "67eb5b9f3bb426e0d0fd8200"
     }
   ])
+  const [bookComments, setBookComments] = useState([])
   const comments = [
     { avatar: 'https://static.ybox.vn/2022/7/4/1658994867129-Spy.x.Family.full.3493446.jpg', user: 'User1', content: 'This is a comment' },
     { avatar: 'https://static.ybox.vn/2022/7/4/1658994867129-Spy.x.Family.full.3493446.jpg', user: 'User2', content: 'This is a comment' },
@@ -45,48 +46,63 @@ function NovelDetails() {
     { avatar: 'https://static.ybox.vn/2022/7/4/1658994867129-Spy.x.Family.full.3493446.jpg', user: 'User4', content: 'This is a comment' },
   ]
   // handle click on a chapter => navigate to MangaReader
-  const handleClickedChapter = (novelid, noveltitle, chapterid, chaptertitle) => {
+  const handleClickedChapter = (noveltitle, chapterid, chapternumber, chaptertitle) => {
     //     /novel/:novelid/:noveltitle/:chapterid/:chaptertitle
-    navigate(`/novelReader/${novelid}/${chapterid}`, { state: { chapters, noveltitle, chaptertitle } })
+    navigate(`/novelReader/${_id}/${chapterid}`, { state: { chapters, noveltitle, chapternumber, chaptertitle } })
   }
-  const handleStartReading = (novelid, noveltitle, chapterid, chaptertitle) => {
-    navigate(`/novelReader/${novelid}/${chapterid}`, { state: { chapters, noveltitle, chaptertitle } })
+  const handleStartReading = (noveltitle, chapterid, chapternumber, chaptertitle) => {
+    navigate(`/novelReader/${_id}/${chapterid}`, { state: { chapters, noveltitle, chapternumber, chaptertitle } })
   }
 
-    //handle add to favorite
-    const handleAddFavorite = async () => {
-      // check if user is logged in
-      if(!user){
-        toast.error("Vui lòng đăng nhập để thêm vào thư viện")
-        return
-      }
-  
-      try {
-        const response = await apiAuth.post('/api/reader/favorite/addFavorite', {bookId: novelid});
-        if (response.data.success) {
-          toast.success("Thêm vào thư viện thành công")
-        } else {
-          toast.error("Thêm vào thư viện thất bại")
-        }
-      } catch (error) {
-        console.log(error)
+  //handle add to favorite
+  const handleAddFavorite = async () => {
+    // check if user is logged in
+    if (!user) {
+      toast.error("Vui lòng đăng nhập để thêm vào thư viện")
+      return
+    }
+
+    try {
+      const response = await apiAuth.post('/api/reader/favorite/addFavorite', { bookId: _id });
+      if (response.data.success) {
+        toast.success("Thêm vào thư viện thành công")
+      } else {
         toast.error("Thêm vào thư viện thất bại")
       }
+    } catch (error) {
+      console.log(error)
+      toast.error("Thêm vào thư viện thất bại")
     }
+  }
 
 
   useEffect(() => {
-    api.get(`/api/novel/${novelid}`).then(res => {
+    api.get(`/api/novel/${_id}`).then(res => {
       console.log(res.data)
       setInfoNovel(res.data.data)
-      api.get(`/api/novel/${novelid}/chapters`).then(res => {
+      api.get(`/api/novel/${_id}/chapters`).then(res => {
         console.log(res.data)
         setChapters(res.data.data.sort((a, b) => a.order - b.order))
       })
     }).catch(err => console.log(err)).finally(() => {
       setLoading(false)
     })
-  }, [novelid])
+  }, [_id])
+
+  // get book comments
+  useEffect(() => {
+    api.get(`api/reader/review/book/${_id}`)
+      .then((res) => {
+        console.log("check /api/reader/review/book/:id", res.data.data);
+        setBookComments(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        // setLoading(false)
+      }).finally(() => {
+        setLoading(false)
+      })
+  }, [])
 
   if (loading) {
     return (
@@ -120,8 +136,8 @@ function NovelDetails() {
 
           <div className='flex-col px-4 md:ml-10 mt-6 md:mt-0'>
             <div className='flex flex-col sm:flex-row justify-center md:justify-start space-y-2 sm:space-y-0 sm:space-x-3 mb-4'>
-              <div onClick={handleAddFavorite} className='rounded bg-green-700 p-2 md:p-3 text-white text-center cursor-pointer font-bold'>Thêm vào thư viện</div>
-              <div onClick={() => handleStartReading(infoNovel._id, infoNovel.title, chapters[0]._id, chapters[0].chapter_title)} className='rounded border bg-white p-2 md:p-3 text-center cursor-pointer font-bold'>Bắt đầu đọc</div>
+              <div onClick={handleAddFavorite} className='rounded bg-green-700 p-2 md:p-3 text-white text-center cursor-pointer font-bold'>Add to favourite</div>
+              <div onClick={() => handleStartReading(infoNovel.title, chapters[0]._id, 1, chapters[0].chapter_title)} className='rounded border bg-white p-2 md:p-3 text-center cursor-pointer font-bold'>Start reading</div>
             </div>
 
             <div className='flex flex-wrap justify-center md:justify-start mb-6 md:mb-4'>
@@ -150,7 +166,7 @@ function NovelDetails() {
           <p className='font-bold text-green-700'><u>Danh sách chương</u></p>
           <ul className='h-64 overflow-y-scroll'>
             {chapters.map((chapter, index) => (
-              <li key={chapter.chapter_title} onClick={() => handleClickedChapter(infoNovel._id, infoNovel.title, chapter._id, chapter.chapter_title)}>
+              <li key={chapter.chapter_title} onClick={() => handleClickedChapter(infoNovel.title, chapter._id, chapter.order, chapter.chapter_title)}>
                 <div className='p-2 border rounded-md m-1 bg-white cursor-pointer'>
                   <p className='line-clamp-1'>{index}. {chapter.chapter_title}</p>
                 </div>
@@ -163,14 +179,12 @@ function NovelDetails() {
         <div className='md:pt-20 md:flex-2 mt-3 md:mt-0'>
           <p className='font-bold text-green-700'><u>Bình luận</u></p>
           <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>
+            {bookComments.map((comment, index) => (
+              <li key={comment._id}>
                 <div className='mt-3'>
-                  <div className='flex items-center'>
-                    <img src={comment.avatar} alt={comment.user} className='w-10 h-10 rounded-full' />
-                    <p className='ml-1'>{comment.user}</p>
-                  </div>
                   <div className='w-[100%] border p-2 rounded-md mt-2'>
+                    <p className='ml-1 font-bold'>{comment.username}</p>
+                    <p className='text-gray-500'>Chapter {comment.chapternumber}: {comment.chaptertitle}</p>
                     <p>{comment.content}</p>
                   </div>
                 </div>
